eebb35169e23f883b48e4eaa00799674314281b1
Apollo Yang <epoluodi@gmail.com>
2017-03-04 14:00:17 +0800

    Initial Commit

---
+++ PlumFlowerMark/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlumFlowerMark
+//
+//  Created by 杨晓光 on 2017/3/4.
+//  Copyright © 2017年 杨晓光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
