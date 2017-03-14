90dcb1a21f517cff9145629c4a788f78e07f6808
epoluodi <12163687@qq.com>
2017-03-14 17:52:21 +0800

    输入备注

---
+++ PlumFlowerMark/TextViewController.h
@@ -0,0 +1,17 @@
+//
+//  TextViewController.h
+//  PlumFlowerMark
+//
+//  Created by Stereo on 2017/3/14.
+//  Copyright © 2017年 杨晓光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextViewController : UIViewController
+
+
+@property (weak, nonatomic) IBOutlet UINavigationItem *navtitle;
+@property (weak, nonatomic) IBOutlet UITextView *txtmemo;
+
+@end
